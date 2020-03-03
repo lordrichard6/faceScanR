a0dc4d749c9d818f8c5d06c584662a747331993b
root <root@RICK.localdomain>
2020-03-03 10:47:24 +0100

    add nav and logo component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navigation.css';
+
+
+const Navigation = () => {
+    return (
+        <nav className='navigation'>
+            <p>Sign Out</p>
+        </nav>    
+    )
+}
+
+export default Navigation
