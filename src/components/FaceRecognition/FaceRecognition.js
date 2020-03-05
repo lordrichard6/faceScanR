e0e58e6f44b206676eb8a6acaf376745ac74bdfc
root <root@RICK.localdomain>
2020-03-05 13:37:07 +0100

    final version 1 face

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+
+const FaceRecognition = ({imageURL, box}) => {
+    return (
+        <div className='FaceRecognition'>
+            <div className='FaceRecognition-inner'>
+                <img id='inputImage' src={imageURL} alt="" width='500px' height='auto' />
+                <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+            </div>
+        </div>
+    )
+}
+
+export default FaceRecognition
